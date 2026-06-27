dffaceb841843ecd965f10a956ada53cb37bdaeb
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-27 12:54:36 +0530

    httpparse

--- BannerGrabber/parser.py
+++ BannerGrabber/parser.py
@@ -8,3 +8,14 @@ def parse_ssh_banner(banner:str) -> dict:
         "software_version":software_version,
         "raw_banner":banner
     }
+def parse_http_banner(banner: str) -> dict:
+    parts = banner.split()
+
+    return {
+        "protocol": parts[0],
+        "status_code": parts[1] if len(parts) > 1 else None,
+        "status": " ".join(parts[2:]) if len(parts) > 2 else None,
+        "raw_banner": banner
+    }
+
+
