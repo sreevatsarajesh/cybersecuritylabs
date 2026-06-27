ec761fc2fa613cd6d7d383acafd5e825c4458bbd
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-27 12:41:01 +0530

    Update banner.py

--- BannerGrabber/banner.py
+++ BannerGrabber/banner.py
@@ -1,7 +1,2 @@
-import socket
-s=socket.socket()
-print ("Socket Created")
-s.connect(("scanme.nmap.org",22))
-banner = s.recv(1024)
-print(banner.decode())
-s.close()
+from scanner import grab_banner
+from parser import parse_banner
