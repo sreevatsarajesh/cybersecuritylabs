209735abc77adaf635dbbeda168fc26a4149a955
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-27 13:17:42 +0530

    Update scanner.py

--- BannerGrabber/scanner.py
+++ BannerGrabber/scanner.py
@@ -2,11 +2,11 @@ import socket
 
 def grab_banner(host: str,port: int,timeout:int=3):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(3)
-
-    sock.connect((host,port))
-
-    banner=sock.recv(1024).decode(errors="ignore")
-
-    sock.close()
-    return banner
+    try:
+        sock.settimeout(timeout)
+        sock.connect((host, port))
+        banner = sock.recv(1024).decode(errors="ignore")
+        return banner
+    finally:
+        sock.close()
+    
