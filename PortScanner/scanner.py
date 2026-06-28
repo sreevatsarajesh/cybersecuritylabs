f7ede43dc84537915893312f6b210e20773cc71e
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-28 10:41:48 +0530

    fixed scanner

--- PortScanner/scanner.py
+++ PortScanner/scanner.py
@@ -4,7 +4,7 @@ def scan_port(host: str, port :int,timeout: int=3):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.settimeout(3)
-        result = sock.connect_ex((HOST,PORT))
+        result = sock.connect_ex((host,port))
         return result==0
     finally:
         sock.close()
