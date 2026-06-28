d646310fe5e079e6ee32711433217f775df5863f
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-28 10:38:21 +0530

    making scanner.py

---
+++ PortScanner/scanner.py
@@ -0,0 +1,10 @@
+import socket
+
+def scan_port(host: str, port :int,timeout: int=3):
+    sock = sock.connect(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.settimeout(3)
+        sock.connect_ex((HOST,PORT))
+        return result==0
+    finally:
+        sock.close()
