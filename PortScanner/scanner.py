fc6f62dd900c91d958476540e9ffe572a4b75a8a
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-28 10:41:17 +0530

    fixting socket

--- PortScanner/scanner.py
+++ PortScanner/scanner.py
@@ -1,7 +1,7 @@
 import socket
 
 def scan_port(host: str, port :int,timeout: int=3):
-    sock = sock.connect(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.settimeout(3)
         result = sock.connect_ex((HOST,PORT))
