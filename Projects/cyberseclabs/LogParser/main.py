419fc775f0c8e3576497d44f759d3f904d91590c
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-29 09:13:38 +0530

    Update main.py

--- Projects/cyberseclabs/LogParser/main.py
+++ Projects/cyberseclabs/LogParser/main.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from parser import parse_line
 
-LOG_FILE="sample_logs/sample.log"
+BASE_DIR = Path(__file__).parent
+LOG_FILE = BASE_DIR / "sample_logs" / "sample.log"
 
-with open(LOG_FILE,"r") as file:
+with LOG_FILE.open("r") as file:
     for line in file:
         parsed = parse_line(line)
-
         print(parsed)
