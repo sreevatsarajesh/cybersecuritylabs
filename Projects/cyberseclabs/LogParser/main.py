d2c593df142825110d78c94f61d5ea1e6e0a6cd9
sreevatsarajesh <sreevatsarajesh@gmail.com>
2026-06-29 08:52:19 +0530

    updating main.py

---
+++ Projects/cyberseclabs/LogParser/main.py
@@ -0,0 +1,9 @@
+from parser import parse_line
+
+LOG_FIlE="sample_logs/sample.log"
+
+with open(LOG_FILE,"r") as file:
+    for line in file:
+        parsed = parse_line(line)
+
+        print(parsed)
